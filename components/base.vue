48c24eb47cb631d15535dd53effa8f275e8addad
Marlon Castillo <58896443+mcastillo1030@users.noreply.github.com>
2023-05-26 12:54:27 -0400

    begin build

---
+++ components/base.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="light">
+		<h1>Heading 1 - Est quis ea ullamco duis incididunt officia cillum</h1>
+		<h2>Heading 2 - Est quis ea ullamco duis incididunt officia cillum</h2>
+		<h3>Heading 3 - Est quis ea ullamco duis incididunt officia cillum</h3>
+		<h4>Heading 4 - Est quis ea ullamco duis incididunt officia cillum</h4>
+		<h5>Heading 5 - Est quis ea ullamco duis incididunt officia cillum</h5>
+		<h6>Heading 5 - Est quis ea ullamco duis incididunt officia cillum</h6>
+	</div>
+	<div class="dark"></div>
+</template>
