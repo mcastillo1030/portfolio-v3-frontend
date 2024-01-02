28026265242e57b7ea845d6a4a874bb92e6f198a
Marlon Castillo <58896443+mcastillo1030@users.noreply.github.com>
2024-01-02 15:53:08 -0500

    also increase tap target

--- components/HomeIntro.vue
+++ components/HomeIntro.vue
@@ -66,7 +66,7 @@
       align-items: center;
       padding: 0;
       margin: 2rem 0 1.5625rem;
-      gap: 1.25rem;
+      gap: .75rem;
       list-style: none;
 
       &::before {
@@ -85,7 +85,7 @@
     }
 
     &__contact-link {
-      padding: .25rem;
+      padding: .75rem;
       transition: color .2s ease-out;
 
       &:hover {
